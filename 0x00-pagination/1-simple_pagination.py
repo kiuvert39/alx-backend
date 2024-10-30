46a52ea1f9ffaac0f3d85c5a93bc52d8e3e1392e
kliuvert <forrexplug@gmail.com>
2024-10-30 03:37:26 -0700

    fixing

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -7,8 +7,9 @@ from typing import List
 def index_range(page: int, page_size: int) -> tuple:
 
     page_start_index = (page - 1) * page_size
-    end_index =  page_start_index + page_size
+    end_index = page_start_index + page_size
     return (page_start_index, end_index)
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -29,8 +30,8 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]
+
             assert type(page) == int and type(page_size) == int
             assert page > 0 and page_size > 0
 
@@ -39,6 +40,4 @@ class Server:
             data = self.dataset()
             if start >= len(data):
                 return []
-            
             return data[start:end]
-            
